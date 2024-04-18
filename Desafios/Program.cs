a3a5cddad6a3ca21e07fcfb579a25440135dd055
hugo de araujo <hugo.saraujo@tutanota.com>
2024-04-18 18:40:44 -0300

    Adicionar arquivos de projeto.

---
+++ Desafios/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/* 
+ * 1 - Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+ * 2 - Mostrar o conteúdo da variável curso no console.
+ * 3 - Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+ * 4 - Mostrar na tela o nome dos instrutores do curso.
+ */
+string curso = "Programação Back-end";
+Console.WriteLine(curso);
+
+string nome = "Stevie";
+string sobrenome = "Nicks";
+
+Console.WriteLine($"{nome} {sobrenome}");
+
+string nomeInstrutor1 = "Guilherme Dias";
+string nomeInstrutor2 = "Daniel Portugal";
+
+Console.WriteLine($"O curso de C# teme como instrutores {nomeInstrutor1} e {nomeInstrutor2}");
