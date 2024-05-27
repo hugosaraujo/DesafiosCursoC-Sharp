fa0d1dcaeed788f88382ccaafad83e2453e1cefe
hugo de araujo <hugo.saraujo@tutanota.com>
2024-05-26 23:11:17 -0300

    Resolução do primeiro desafio do curso 1 de C#

---
+++ Aula01-CriandoPrimeiroPrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+    1- Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+    2- Mostrar o conteúdo da variável curso no console.
+    3- Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+    4- Mostrar na tela o nome dos instrutores do curso.
+*/
+
+string curso = "C# Fundamentos";
+Console.WriteLine(curso);
+
+string nome = "Anthony"; 
+string sobrenome = "Edwards";
+string nomeCompleto = nome + " " + sobrenome;
+Console.WriteLine(nomeCompleto);
+
+
+string instrutorGui = "Guilherme";
+string instrutorDaniel = "Daniel";
+
+Console.WriteLine($"Curso {curso} com os instrutores {instrutorDaniel} e {instrutorGui}");
---
+++ ListasLoops/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * 1 - Escrever uma função que a partir de dois números de ponto flutuante a e b exiba no console o resultado de suas quatro operações básicas (adição, subtração, divisão e multiplicação), utilizando interpolação de strings.
+ * 2 - Criar uma lista de bandas vazia e adicionar suas bandas prediletas em seguida.
+ * 3 - Utilizar a estrutura 'for' para mostrar todas as suas bandas preferidas, listadas na lista do exercício anterior, no console.
+ * 4 - Criar um programa que calcula a soma de todos os elementos inteiros em uma lista.
+ */
+
+using System;
+
+void calcula(double a, double b)
+{
+    Console.Write("Selecione a operação matemática: ");
+    int operacaoSelecionada = int.Parse(Console.ReadLine()!);
+
+    exibeMenuDeOperacoes();
+
+    switch (operacaoSelecionada)
+    {
+        case 1:
+            double soma = a + b; 
+            Console.WriteLine($"{a} + {b} = {soma}");
+            break;
+        case 2:
+            double subtracao = a - b;
+            Console.WriteLine($"{a} - {b} = {subtracao}");
+            break;
+        case 3:
+            double multiplicacao = a * b;
+            Console.WriteLine($"{a} X {b} = {multiplicacao}");
+            break;
+        case 4:
+            double divisao = a / b;
+            Console.WriteLine($"{a} + {b} = {divisao}");
+            break;
+        default: 
+            Console.WriteLine("Não é uma opção válida");
+            break;
+    }
+}
+
+void exibeMenuDeOperacoes()
+{
+    Console.WriteLine("(1) - Soma");
+    Console.WriteLine("(2) - Subtração");
+    Console.WriteLine("(3) - Multiplicação");
+    Console.WriteLine("(4) - Divisão");
+
+}
