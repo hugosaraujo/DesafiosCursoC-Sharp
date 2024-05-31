ac86946792d02c69db7cc5f5981e789d8217641d
hugo de araujo <hugo.saraujo@tutanota.com>
2024-05-30 21:41:52 -0300

    Resposta de mais um exercício. Crição de uma lista contadora de inteiros. Aplicando um filtro para que seja pulado os números ímpares.

---
+++ Contador/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> inteiros = new List<int>();
+int limite = 10;
+
+for (int i = 0; i <= limite; i++)
+{
+    if (i % 2 != 0)
+    {
+        i++;
+    }
+    inteiros.Add(i);
+}
+
+foreach (int inteiro in inteiros)
+{
+    Console.Write($"{inteiro}, ");
+}
