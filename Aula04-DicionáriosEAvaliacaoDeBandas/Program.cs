15a1131d481398dd1f3f0b6667cdc005b8903f0b
hugo de araujo <hugo.saraujo@tutanota.com>
2024-06-09 10:23:29 -0300

    Respostas -> respondendo as perguntas 3 e 4 do desafio

--- Aula04-DicionáriosEAvaliacaoDeBandas/Program.cs
+++ Aula04-DicionáriosEAvaliacaoDeBandas/Program.cs
@@ -6,8 +6,22 @@
     
 */
 
-//Resolução pergunta 3
+//Resolução da pergunta 1
+Dictionary<string, List<double>> alunos = new Dictionary<string, List<double>>()
+{
+    {"Steve", new List<double>{10, 9, 9 }}
+};
+alunos.Add("Alice", new List<double>{10, 9, 6});
+alunos.Add("João", new List<double>{8, 9.5, 6.5});
 
+foreach (var aluno in alunos)
+{
+    string nomeAluno = aluno.Key;
+    var listaDeNotas = aluno.Value;
+    double mediaNotas = listaDeNotas.Average();
+
+    Console.WriteLine($"A média do {nomeAluno} é {mediaNotas}");
+}
 
 //Resolução pergunta 2
 Dictionary<string, int> listaDeProdutos = new Dictionary<string, int>();
@@ -21,19 +35,57 @@ foreach (var produto in listaDeProdutos)
     Console.WriteLine($"{nomeProduto} = {quantidadeEmEstoque} unidades");
 }
 
-//Resolução da pergunta 1
-Dictionary<string, List<double>> alunos = new Dictionary<string, List<double>>()
+//Resolução pergunta 3
+Dictionary<string, string> quiz = new Dictionary<string, string>()
 {
-    {"Steve", new List<double>{10, 9, 9 }}
+    { "Qual a moeda predominante da União Europeia?", "Euro" },
+    { "Quem ganhou a copa do mundo de 2022", "Argentina" },
+    { "Qual o maior dos oceanos", "Pacífico" }
 };
-alunos.Add("Alice", new List<double>{10, 9, 6});
-alunos.Add("João", new List<double>{8, 9.5, 6.5});
 
-// foreach (var aluno in alunos)
-// {
-//     string nomeAluno = aluno.Key;
-//     var listaDeNotas = aluno.Value;
-//     double mediaNotas = listaDeNotas.Average();
-//
-//     Console.WriteLine($"A média do {nomeAluno} é {mediaNotas}");
-// }
+foreach (var questao in quiz)
+{
+    string pergunta = questao.Key;
+    string resposta = questao.Value;
+
+    Console.WriteLine("Tente acertar as seguinte questão!");
+    Console.WriteLine(pergunta);
+    Console.Write("Resposta: ");
+    string respostaUsuario = Console.ReadLine()!;
+    if (respostaUsuario.Equals(resposta, StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("Tu acertou! Parabéns");
+    }
+    else
+    {
+        Console.WriteLine("Errou!!!");
+        Console.WriteLine($"A resposta certa era {resposta}\n");
+    }
+}
+
+//resolução pergunta 4
+Dictionary<string, string> loginESenhaCadastrados = new Dictionary<string, string>();
+
+string login = String.Empty;
+string senha = String.Empty;
+string sair = String.Empty;
+
+while (!sair.Equals("N", StringComparison.OrdinalIgnoreCase))
+{
+    Console.Write("Preencha o seu login: ");
+    login = Console.ReadLine()!;
+
+    Console.Write("Insira a sua senha: ");
+    senha = Console.ReadLine()!;
+
+    loginESenhaCadastrados.Add(login, senha);
+
+    Console.Write("Deseja adicionar outro login e senha? [S/N] ");
+    sair = Console.ReadLine()!;
+}
+
+Console.WriteLine("Encerrando a aplicação!!");
+foreach (var loginESenha in loginESenhaCadastrados)
+{
+    Console.WriteLine($"Login:{login}, Senha: {senha}");
+}
